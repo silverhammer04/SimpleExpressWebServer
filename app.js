df787f26d70d2db75cb94e4ea266941a8c3b1db3
Maxwell Seifert <silverhammer04@yahoo.com>
2020-09-04 08:42:54 -0600

    commit before submit

--- app.js
+++ app.js
@@ -22,31 +22,46 @@ const products = [
         name: "Bidet",
         price: 11.98,
         quantity: 4000
-    }
+    },
 ]
 
+app.use(express.json());
 app.get('/', (req, res) => res.send('Hello World! with nodemon'));
 
 //app.METHOD(PATH, HANDLER)
 //GET at /products
-app.get('/products', (req, res) => res.send('Get at /products'));
+app.get('/products', (req, res) => res.send(products));
 
 //POST at /products
-app.post('/products', (req, res) => res.send('POST at /products'));
+app.post('/products', (req, res) => {
+    const body = req.body;
+    products.push(body);
+    res.send(products[products.length - 1])
+});
 
 //PUT at /products/{productID}
-app.put('/products/:productID', (req, res) => 
-    res.send(`PUT at/products/${req.params.productID}`)
-    );
+app.put('/products/:productID', (req, res) => {
+    const index = req.params.productID;
+    const body = req.body;
+    products.splice(index, 1, body);
+    res.send(products[index]);
+});
 
 //PATCH at /products/{productID}
-app.patch('/products/:productID', (req, res)=>
-    res.send(`PATCH at /products/${req.params.procuctID}`)
-    );
+app.patch('/products/:productID', (req, res)=> {
+    const index = req.params.productID;
+    const body = req.body;
+    const keys = Object.keys(body);
+    keys.forEach(key => products[index][key] = body[key]);
+    res.send(products[index])
+});
 
 //DELET  at /products/{productID}
-app.delete('/products/:productID', (req, res) =>
-    res.send(`DELETE at /products/${req.params.productID}`)
-    );
+app.delete('/products/:productID', (req, res) => {
+    const index = req.params.productID;
+    const deletedProduct = products[index];
+    products.splice(index, 1);
+    res.send(deletedProduct)
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
